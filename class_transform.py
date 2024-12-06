a7abee7bb93f77d5777149bef79069d08c385550
Yen Nguyen <haiyen190903@gmail.com>
2024-12-06 11:58:17 +0700

    .

--- class_transform.py
+++ class_transform.py
@@ -5,21 +5,31 @@ import re
 
 class Transform:
     """
-    Transforming a log file into a DataFrame involves reading the log file, parsing the necessary information, 
-    and converting it into a structured format suitable for data analysis using a tool like Pandas, Vaex, ... 
-    Pandas DataFrame is a two-dimensional size-mutable, potentially heterogeneous tabular data structure with 
-    labeled axes (rows and columns).
-    Vaex is a powerful Python library designed for handling large tabular datasets efficiently. It provides 
-    out-of-core dataframes, which means it can process data that doesn't fit into memory by reading and processing 
-    data in chunks. This makes it particularly useful for working with big data.
+    Transforming a log file into a DataFrame involves reading 
+    the log file, parsing the necessary information, and 
+    converting it into a structured format suitable for 
+    data analysis using a tool like Pandas, Vaex, ... 
+    Pandas DataFrame is a two-dimensional size-mutable, potentially 
+    heterogeneous tabular data structure with labeled axes 
+    (rows and columns).
+    Vaex is a powerful Python library designed for handling 
+    large tabular datasets efficiently. It provides out-of-core 
+    dataframes, which means it can process data that 
+    doesn't fit into memory by reading and processing 
+    data in chunks. This makes it particularly useful for 
+    working with big data.
 
-    Input: File data about Server log with 10 types information, including: std, remote_address, remote_user, 
-    datetime, method, path, header,	status,	bytes_sent,	referer	user_agent 
+    Input: File data about Server log with 10 types information, 
+    including: std, remote_address, remote_user, datetime, method, 
+    path, header,	status,	bytes_sent,	referer	user_agent 
     
     from parse: 
-    %{IP:remote_address} - %{DATA:remote_user} \[%{HTTPDATE:time_local}\] \"%{WORD:http_method} %{URIPATHPARAM:path} 
-    %{DATA:header}\" %{NUMBER:status} %{NUMBER:body_bytes_sent:long} \"%{DATA:http_referer}\" \"%{DATA:http_user_agent}\" 
-    kong_request_id: \"%{WORD:kong_request_id}\"
+    %{IP:remote_address} - %{DATA:remote_user} 
+    \[%{HTTPDATE:time_local}\] \"%{WORD:http_method}
+    %{URIPATHPARAM:path} %{DATA:header}\" %{NUMBER:status} 
+    %{NUMBER:body_bytes_sent:long} \"%{DATA:http_referer}\" 
+    \"%{DATA:http_user_agent}\" kong_request_id: 
+    \"%{WORD:kong_request_id}\"
 
     Output: A Pandas or Vaex Dataframe includes 10 columns 
     """
@@ -28,21 +38,24 @@ class Transform:
     def read_log_file(file_path, pattern):
         with open(file_path, 'r') as file:
             log_lines = file.readlines()
-            parsed_logs = [re.match(pattern, line).groupdict() for line in log_lines if re.match(pattern, line)]
+            parsed_logs = [re.match(pattern, line).groupdict() 
+                           for line in log_lines if re.match(pattern, line)]
         return parsed_logs
     
     def transform(parsed_logs):
         df = pd.DataFrame(parsed_logs)
         return df
 
-parsed_logs = Transform.read_log_file(Transform.file_log, Transform.log_pattern) 
+parsed_logs = Transform.read_log_file(Transform.file_log, 
+                                      Transform.log_pattern) 
 df = Transform.transform(parsed_logs) 
 
 #Data Preprocessing
 
 df['std'] = df['std'].replace({'stdout': 0, 'stderr': 1})
 from datetime import datetime
-df['datetime'] = df['datetime'].apply(lambda x: datetime.strptime(x, "%d/%b/%Y:%H:%M:%S %z"))
+df['datetime'] = df['datetime'].apply(lambda x: datetime.strptime
+                                      (x, "%d/%b/%Y:%H:%M:%S %z"))
 df = df.drop(['remote_user'], axis = 1)
 df = df.drop(['header'], axis = 1)
 df['method'] = df['method'].replace({'GET': 0, 'POST': 1})
@@ -52,5 +65,5 @@ df['day'] = df['datetime'].dt.day
 df['hour'] = df['datetime'].dt.hour
 df['month'] = df['datetime'].dt.month
 df.isnull().sum()
-
 print (df.head())
+#df.to_csv ("D:\code-python\project_practice\server_log\dataframe\data_after_preprocessing.csv")
--- class_visualize.py
+++ class_visualize.py
@@ -4,17 +4,21 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 class Visualize:
     """
-    Visualization refers to the process of representing data or information in a visual format such as charts,
-    graphs, maps, and other visual aids. The goal of visualization is to make complex data more accessible, 
-    understandable, and usable, often revealing patterns, trends, and insights that might not be immediately 
-    apparent from raw data alone. 
+    Visualization refers to the process of representing data or 
+    information in a visual format such as charts, graphs, maps, 
+    and other visual aids. The goal of visualization is to make 
+    complex data more accessible, understandable, and usable, 
+    often revealing patterns, trends, and insights that might not 
+    be immediately apparent from raw data alone. 
 
     Types of Visualization:
 
-    Charts and Graphs: Bar charts, line graphs, pie charts, histograms, etc.
+    Charts and Graphs: Bar charts, line graphs, pie charts, 
+    histograms, etc.
     Maps: Geographical data visualization, heat maps.
     Diagrams: Flowcharts, network diagrams, tree diagrams.
-    Interactive Visuals: Dashboards, interactive maps, and graphs that allow user interaction.
+    Interactive Visuals: Dashboards, interactive maps, and graphs 
+    that allow user interaction.
 
     Tools and Libraries:
 
@@ -22,42 +26,78 @@ class Visualize:
     Software and Platforms: Tableau, Power BI, Google Data Studio.
     
     Input: DataFrame Server Log from class Transform
-    Output: Graphs about frequency; Heat maps about correlation about features
+    Output: Graphs about frequency; Heat maps about correlation 
+    about features
     """
     def read():
         df = pd.read_csv(r"D:\code-python\project_practice\server_log\dataframe\data_after_preprocessing.csv")
         return df
-df = Main.read()
+df = Visualize.read()
 df = df.drop(['Unnamed: 0'], axis =1)
 #Visualization
+
+#Frequency of Status between 400 and 500
 df_filter = df [(df['status']>=400) & (df['status']<500)]
 plt.figure(figsize=(10,6))
-sns.histplot(df_filter['status'], bins =30, kde = True, color = 'skyblue')
+sns.histplot(df_filter['status'], bins =30, kde = True, 
+             color = 'skyblue')
 plt.xlabel('Status')
 plt.ylabel('Frequency')
 plt.title('Frequency of Status between 400 and 500')
 plt.grid()
 plt.show()
 
+#Frequency of Bytes Sent Among Status between 400 and 500
 plt.figure(figsize=(10,6))
-sns.histplot(df_filter['bytes_sent'], bins =30, kde = True, color = 'white')
-for p in plot.patches: 
-    plot.annotate(format(p.get_height(), '.0f'), 
-    (p.get_x() + p.get_width() / 2., p.get_height()), ha = 'center', 
-    va = 'center', xytext = (0, 10), textcoords = 'offset points')
+sns.histplot(df_filter['bytes_sent'], bins =30, kde = True, 
+             color = 'orange')
 plt.xlabel('Bytes Sent')
 plt.ylabel('Frequency')
-plt.title('Frequency of Bytes Sent Among Status between 400 and 500')
+plt.title('Frequency of Bytes Sent Among Status ' 
+          'between 400 and 500')
 plt.grid()
 plt.show()
 
-#Visualization
+#Frequency of Hour Among Status between 400 and 500
+plt.figure(figsize=(10,6))
+sns.histplot(df_filter['hour'], bins =30, kde = True, 
+             color = 'skygreen')
+plt.xlabel('Hour')
+plt.ylabel('Frequency')
+plt.title('Frequency of Hour Among Status ' 
+          'between 400 and 500')
+plt.grid()
+plt.show()
+
+#Frequency of Hour Among Status between 400 and 500
+plt.figure(figsize=(10,6))
+sns.histplot(df_filter['user_agent'], bins =30, kde = True, 
+             color = 'green')
+plt.xlabel('User Agent')
+plt.ylabel('Frequency')
+plt.title('Frequency of User Agent Among Status ' 
+          'between 400 and 500')
+plt.grid()
+plt.show()
+
+#Frequency of Status equal and greater than 500
 df_filter1 = df [df['status']>=500]
 plt.figure(figsize=(10,6))
-sns.histplot(df_filter1['status'], bins =30, kde = True, color = 'red')
+sns.histplot(df_filter1['status'], bins =30, kde = True,
+              color = 'red')
 plt.xlabel('Status')
 plt.ylabel('Frequency')
 plt.title('Frequency of Status equal and greater than 500')
 plt.grid()
 plt.show()
 
+#Frequency of Bytes Sent Among Status equal and greater than 500
+plt.figure(figsize=(10,6))
+sns.histplot(df_filter1['bytes_sent'], bins =30, kde = True, 
+             color = 'orange')
+plt.xlabel('Bytes Sent')
+plt.ylabel('Frequency')
+plt.title('Frequency of Bytes Sent Among Status ' 
+          'equal and greater than 500')
+plt.grid()
+plt.show()
